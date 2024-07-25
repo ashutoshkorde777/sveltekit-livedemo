564d31d3bd6d5fde01c17114db0511cef786d6f8
Ashutosh Korde <ashutoshkorde777@gmail.com>
2024-07-26 02:16:11 +0530

    added

---
+++ src/routes/blog/+page.server.js
@@ -0,0 +1,7 @@
+import { sql } from "@vercel/postgres";
+
+export async function load({ params, locals }) {
+  return {
+    names: await sql`SELECT * from NAMES where user_id='${locals.user}'`
+  }
+}
---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let data;
+</script>
+
+<div>
+  {#each data.names as {name}}
+    <div class="font-semibold">{name}</div>
+  {/each}
+</div>
